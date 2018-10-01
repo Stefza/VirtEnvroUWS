394cb4c82a27952792928efb567ed192af8eb901
Stefza <43515519+Stefza@users.noreply.github.com>
2018-10-01 20:49:37 +0100

    Scrip Changes

---
+++ Virtual World/Assets/script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// This is a test comment
+public class script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
